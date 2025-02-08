8ed0130ce1258c2b2089b5ebb5b1aa8f7ceb5348
Hersh Krishna <hershkrishna@protonmail.ch>
2025-02-07 21:16:49 -0600

    Create a device

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@
 #![warn(
     unsafe_op_in_unsafe_fn,
     clippy::undocumented_unsafe_blocks,
-    clippy::missing_docs_in_private_items
+    missing_debug_implementations
 )]
+
+// // Uncomment to check for undocumented stuff
+//#![warn(clippy::missing_docs_in_private_items)]
 
 /// Current Top Level crate for our app. Will break up in future
 use std::{
@@ -18,8 +21,10 @@ use std::{
 use app_dirs2::AppInfo;
 
 use ash::vk::{
-    Bool32, DebugUtilsMessageSeverityFlagsEXT, DebugUtilsMessageTypeFlagsEXT,
-    DebugUtilsMessengerCallbackDataEXT, DebugUtilsMessengerEXT, SurfaceKHR,
+    self, Bool32, DebugUtilsMessageSeverityFlagsEXT, DebugUtilsMessageTypeFlagsEXT,
+    DebugUtilsMessengerCallbackDataEXT, DebugUtilsMessengerEXT, DeviceCreateInfo,
+    DeviceQueueCreateInfo, PhysicalDeviceFeatures2, PhysicalDeviceVulkan12Features,
+    PhysicalDeviceVulkan13Features, QueueFlags, SurfaceKHR,
 };
 use clap::Parser;
 use thiserror::Error;
@@ -39,6 +44,7 @@ const APP_INFO: AppInfo = AppInfo {
 };
 
 /// State of the application while running
+#[derive(Debug)]
 struct RunningState {
     /// Window that we are rendering to
     win: Arc<Window>,
@@ -46,23 +52,28 @@ struct RunningState {
     instance: Arc<Instance>,
     /// Surface corresponding to window
     _surface: Arc<Surface>,
+    device: Arc<Device>,
 }
 
 /// State of the application while suspended
+#[derive(Debug)]
 struct SuspendedState {
     /// Application window that is suspended
     win: Arc<Window>,
     /// Our vk instance
     instance: Arc<Instance>,
+    device: Arc<Device>,
 }
 
 /// State of the app before initialization
+#[derive(Debug)]
 struct UninitState {
     /// Our vk instance (contains pre-initialization stuff safe to make before we have a window)
     instance: Arc<Instance>,
 }
 
 /// Enumeration of the app state. Represents an FSM
+#[derive(Debug)]
 enum AppState {
     /// Uninitialized. Used before we enter the main loop.
     Uninit(UninitState),
@@ -79,6 +90,7 @@ enum AppState {
 /// Wrapper for AppState. Wraps in an option and allows operations like taking
 /// temporary Ownership of the AppState. THIS SHOULD NEVER BE NONE WHEN CONTROL
 /// IS RETURNED TO THE EVENT LOOP
+#[derive(Debug)]
 struct AppRunner {
     /// Internal app_state. Should never be None outside of methods on AppRunner.
     app_state: Option<AppState>,
@@ -206,14 +218,28 @@ impl ApplicationHandler for AppRunner {
                     return;
                 }
             };
+            let device = match Device::create_compatible(&instance, &surface) {
+                Ok(d) => Arc::new(d),
+                Err(e) => {
+                    tracing::error!("Could not create device: Error {}", e);
+                    event_loop.exit();
+                    return;
+                }
+            };
 
             self.app_state = Some(AppState::Running(RunningState {
                 win,
                 instance,
                 _surface: surface,
+                device,
             }))
         }
-        if let Some(SuspendedState { win, instance }) = self.take_suspended() {
+        if let Some(SuspendedState {
+            win,
+            instance,
+            device,
+        }) = self.take_suspended()
+        {
             let surface = match Surface::from_winit_window(&win, &instance) {
                 Ok(s) => Arc::new(s),
                 Err(e) => {
@@ -226,6 +252,7 @@ impl ApplicationHandler for AppRunner {
                 win,
                 instance,
                 _surface: surface,
+                device,
             }));
         }
 
@@ -237,10 +264,15 @@ impl ApplicationHandler for AppRunner {
             instance,
             win,
             _surface: _,
+            device,
         }) = self.take_running()
         {
             event_loop.set_control_flow(event_loop::ControlFlow::Wait);
-            self.app_state = Some(AppState::Suspended(SuspendedState { win, instance }))
+            self.app_state = Some(AppState::Suspended(SuspendedState {
+                win,
+                instance,
+                device,
+            }))
         }
 
         event_loop.set_control_flow(event_loop::ControlFlow::Wait);
@@ -255,6 +287,7 @@ impl ApplicationHandler for AppRunner {
             win,
             instance: _,
             _surface: _,
+            device: _,
         }) = self.as_running_mut()
         {
             match event {
@@ -407,7 +440,7 @@ impl Drop for Instance {
             };
         };
         //SAFETY: Last use of Instance, all children destroyed
-        unsafe { self.destroy_instance(None) };
+        unsafe { self.instance.destroy_instance(None) };
     }
 }
 impl Debug for Instance {
@@ -416,14 +449,6 @@ impl Debug for Instance {
             .field("entry", &"{entry}")
             .field("instance", &self.instance.handle())
             .finish()
-    }
-}
-
-impl std::ops::Deref for Instance {
-    type Target = ash::Instance;
-
-    fn deref(&self) -> &Self::Target {
-        &self.instance
     }
 }
 
@@ -624,6 +649,12 @@ impl Instance {
             debug_messenger,
         })
     }
+
+    /// Get a Vec of all physical devices available to this instance
+    fn enumerate_physical_devices(&self) -> ash::prelude::VkResult<Vec<vk::PhysicalDevice>> {
+        // SAFETY: Effectively made safe by ash managing the memory for us
+        unsafe { self.instance.enumerate_physical_devices() }
+    }
 }
 
 /// Debug Utils Messenger callback. Not for external calling
@@ -677,10 +708,21 @@ struct Surface {
     surface: SurfaceKHR,
     /// Reference counted pointer to the instance. Here for RAII purposes as
     /// Instance must be destroyed *after* surface
-    _parent_instance: Arc<Instance>,
+    parent_instance: Arc<Instance>,
     /// Reference counted pointer to the underlying window. Here for RAII
     /// purposes as Window must be destroyed *after* surface
     _parent_window: Arc<Window>,
+}
+
+impl Debug for Surface {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Surface")
+            .field("surface_instance", &"...")
+            .field("surface", &self.surface)
+            .field("_parent_instance", &self.parent_instance)
+            .field("_parent_window", &self._parent_window)
+            .finish()
+    }
 }
 
 impl Drop for Surface {
@@ -710,14 +752,15 @@ impl Surface {
         win: &Arc<Window>,
         instance: &Arc<Instance>,
     ) -> Result<Self, SurfaceCreateError> {
-        let surface_instance = ash::khr::surface::Instance::new(&instance.entry, instance);
+        let surface_instance =
+            ash::khr::surface::Instance::new(&instance.entry, &instance.instance);
         //SAFETY: Passing a valid window and display handle. Enforces the
         //parent/child relationship between the surface and the instance by
         //holding onto an Arc to the instance
         let surface = unsafe {
             ash_window::create_surface(
                 &instance.entry,
-                instance,
+                &instance.instance,
                 win.display_handle().unwrap().as_raw(),
                 win.window_handle().unwrap().as_raw(),
                 None,
@@ -727,8 +770,299 @@ impl Surface {
         Ok(Self {
             surface_instance,
             surface,
-            _parent_instance: instance.clone(),
+            parent_instance: instance.clone(),
             _parent_window: win.clone(),
         })
+    }
+}
+
+/// Represents a VkDevice
+struct Device {
+    /// Parent for RAII purposes
+    parent: Arc<Instance>,
+    ///Underlying VkDevice
+    device: ash::Device,
+    /// A set of function pointers for interacting with the DebugMessenger in parent
+    debug_utils_fps: Option<ash::ext::debug_utils::Device>,
+}
+
+impl Debug for Device {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Device")
+            .field("parent", &self.parent)
+            .field("device", &self.device.handle())
+            .field(
+                "debug_utils_fps",
+                &if self.debug_utils_fps.is_some() {
+                    "present"
+                } else {
+                    "absent"
+                },
+            )
+            .finish()
+    }
+}
+
+#[derive(Error, Debug)]
+enum DeviceCreateError {
+    #[error("Unknown vulkan error {0}")]
+    /// An error from vulkan that has not yet been categorized
+    UnknownVulkan(ash::vk::Result),
+    #[error("Surface was not created from Instance")]
+    /// You passed in a Surface that wasn't created from Instance
+    InstanceSurfaceMismatch,
+    #[error("No suitable device found")]
+    /// When we went to scan for devices, we didn't find one that fulfilled our
+    /// requirements
+    NoSuitableDevice,
+}
+
+impl From<ash::vk::Result> for DeviceCreateError {
+    fn from(value: ash::vk::Result) -> Self {
+        Self::UnknownVulkan(value)
+    }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct ScoredPhysDev<'a> {
+    phys_dev: vk::PhysicalDevice,
+    score: u32,
+    features_11: vk::PhysicalDeviceVulkan11Features<'a>,
+    ext_props: Vec<vk::ExtensionProperties>,
+    features_10: vk::PhysicalDeviceFeatures,
+    features_12: vk::PhysicalDeviceVulkan12Features<'a>,
+    features_13: vk::PhysicalDeviceVulkan13Features<'a>,
+    props_10: vk::PhysicalDeviceProperties,
+    props_11: vk::PhysicalDeviceVulkan11Properties<'a>,
+    props_12: vk::PhysicalDeviceVulkan12Properties<'a>,
+    props_13: vk::PhysicalDeviceVulkan13Properties<'a>,
+    surface_capabilities: Result<vk::SurfaceCapabilitiesKHR, vk::Result>,
+    graphics_qfi: u32,
+    present_modes: Result<Vec<vk::PresentModeKHR>, vk::Result>,
+    formats: Vec<vk::SurfaceFormatKHR>,
+}
+impl Device {
+    /// Create a device that is compatible with the surface
+    fn create_compatible(
+        instance: &Arc<Instance>,
+        surface: &Surface,
+    ) -> Result<Self, DeviceCreateError> {
+        use DeviceCreateError as Error;
+        if !Arc::ptr_eq(instance, &surface.parent_instance) {
+            return Err(Error::InstanceSurfaceMismatch);
+        }
+        let phys_devs = instance.enumerate_physical_devices()?;
+        let scored_phys_dev: ScoredPhysDev =
+            match phys_devs.into_iter().fold(None, |best_so_far, current| {
+                // SAFETY: current is a VkPhysicalDevice derived from instance
+                let ext_props = match unsafe {
+                    instance
+                        .instance
+                        .enumerate_device_extension_properties(current)
+                } {
+                    Ok(exts) => exts,
+                    Err(_) => {
+                        return best_so_far;
+                    }
+                };
+
+                let mut props_11 = vk::PhysicalDeviceVulkan11Properties::default();
+                let mut props_12 = vk::PhysicalDeviceVulkan12Properties::default();
+                let mut props_13 = vk::PhysicalDeviceVulkan13Properties::default();
+                let mut props = vk::PhysicalDeviceProperties2::default()
+                    .push_next(&mut props_11)
+                    .push_next(&mut props_12)
+                    .push_next(&mut props_13);
+
+                // SAFETY: current is derived from instance. Valid current_dev_props
+                unsafe {
+                    instance
+                        .instance
+                        .get_physical_device_properties2(current, &mut props)
+                };
+
+                let mut current_dev_mem_props = vk::PhysicalDeviceMemoryProperties2::default();
+
+                // Safety: current is derived from instance
+                unsafe {
+                    instance.instance.get_physical_device_memory_properties2(
+                        current,
+                        &mut current_dev_mem_props,
+                    );
+                }
+
+                // SAFETY: Surface and current are both derived from instance
+                let surface_capabilities = unsafe {
+                    surface
+                        .surface_instance
+                        .get_physical_device_surface_capabilities(current, surface.surface)
+                };
+
+                // Safety: surface and current are derived from instance
+                let formats = unsafe {
+                    surface
+                        .surface_instance
+                        .get_physical_device_surface_formats(current, surface.surface)
+                }
+                .expect("What do you mean we can't get surface formats?");
+
+                // SAFETY: current and surface are derived from the same instance
+                let present_modes = unsafe {
+                    surface
+                        .surface_instance
+                        .get_physical_device_surface_present_modes(current, surface.surface)
+                };
+
+                let mut features_11 = vk::PhysicalDeviceVulkan11Features::default();
+                let mut features_12 = vk::PhysicalDeviceVulkan12Features::default();
+                let mut features_13 = vk::PhysicalDeviceVulkan13Features::default();
+                let mut features = vk::PhysicalDeviceFeatures2::default()
+                    .push_next(&mut features_11)
+                    .push_next(&mut features_12)
+                    .push_next(&mut features_13);
+
+                // SAFETY: current is derived from instance
+                unsafe {
+                    instance
+                        .instance
+                        .get_physical_device_features2(current, &mut features);
+                }
+
+                let props_10 = props.properties;
+                let props_11 = props_11;
+                let props_12 = props_12;
+                let props_13 = props_13;
+
+                let features_10 = features.features;
+                let features_11 = features_11;
+                let features_12 = features_12;
+                let features_13 = features_13;
+                //SAFETY: Basically handled by ash doing the memory management for us
+                let qfis = unsafe {
+                    instance
+                        .instance
+                        .get_physical_device_queue_family_properties(current)
+                };
+                let graphics_qfi = qfis.iter().cloned().enumerate().find(|(qfi, props)| {
+                    // SAFETY: current and surface come from the same instance
+                    let present_support = unsafe {
+                        surface
+                            .surface_instance
+                            .get_physical_device_surface_support(
+                                current,
+                                *qfi as u32,
+                                surface.surface,
+                            )
+                    }
+                    .unwrap();
+                    props.queue_flags.contains(QueueFlags::GRAPHICS) && present_support
+                });
+
+                let ext_names_iter = ext_props.iter().map(|ext| {
+                    ext.extension_name_as_c_str()
+                        .expect("What do you mean vulkan didn't give us valid C Strings?")
+                });
+
+                let swapchain_ext_present = ext_names_iter
+                    .clone()
+                    .any(|ext_name| ext_name.eq(ash::khr::swapchain::NAME));
+
+                //Check for mandatory features. This is currently a sample from
+                //vkguide.
+                if features_13.synchronization2 == vk::FALSE
+                    || features_13.dynamic_rendering == vk::FALSE
+                    || features_12.descriptor_indexing == vk::FALSE
+                    || features_12.buffer_device_address == vk::FALSE
+                    || !swapchain_ext_present
+                    || graphics_qfi.is_none()
+                {
+                    return best_so_far;
+                }
+                //SAFETY: We know that graphics_qfi.is_some is true. Established
+                //in the if statement for features
+                let graphics_qfi = unsafe { graphics_qfi.unwrap_unchecked() }.0 as u32;
+
+                //Preliminary scoring, not yet final
+                let device_type_score = match props_10.device_type {
+                    vk::PhysicalDeviceType::DISCRETE_GPU => 4,
+                    vk::PhysicalDeviceType::OTHER | vk::PhysicalDeviceType::INTEGRATED_GPU => 3,
+                    vk::PhysicalDeviceType::VIRTUAL_GPU => 2,
+                    vk::PhysicalDeviceType::CPU => 0,
+                    _ => unreachable!(),
+                };
+                Some(ScoredPhysDev {
+                    phys_dev: current,
+                    score: device_type_score,
+                    ext_props,
+                    features_10,
+                    features_11,
+                    features_12,
+                    features_13,
+                    props_10,
+                    props_11,
+                    props_12,
+                    props_13,
+                    surface_capabilities,
+                    formats,
+                    present_modes,
+                    graphics_qfi,
+                })
+            }) {
+                Some(spd) => spd,
+                None => {
+                    return Err(Error::NoSuitableDevice);
+                }
+            };
+
+        let enabled_extensions = vec![ash::khr::swapchain::NAME.as_ptr()];
+
+        let mut features_12 = PhysicalDeviceVulkan12Features::default()
+            .buffer_device_address(true)
+            .descriptor_indexing(true);
+
+        let mut features_13 = PhysicalDeviceVulkan13Features::default()
+            .dynamic_rendering(true)
+            .synchronization2(true);
+
+        let mut features = PhysicalDeviceFeatures2::default()
+            .push_next(&mut features_12)
+            .push_next(&mut features_13);
+
+        let qcis = [DeviceQueueCreateInfo::default()
+            .queue_family_index(scored_phys_dev.graphics_qfi)
+            .queue_priorities(&[1.0])];
+
+        let dci = DeviceCreateInfo::default()
+            .enabled_extension_names(&enabled_extensions)
+            .push_next(&mut features)
+            .queue_create_infos(&qcis);
+
+        //SAFETY: Valid ci. phys_dev is derived from instance
+        let device = unsafe {
+            instance
+                .instance
+                .create_device(scored_phys_dev.phys_dev, &dci, None)
+        }?;
+
+        let debug_utils_fps = instance
+            .debug_messenger
+            .is_some()
+            .then(|| ash::ext::debug_utils::Device::new(&instance.instance, &device));
+
+        Ok(Self {
+            parent: instance.clone(),
+            device,
+            debug_utils_fps,
+        })
+    }
+}
+
+impl Drop for Device {
+    fn drop(&mut self) {
+        self.debug_utils_fps.take();
+
+        //SAFETY: Last use of device. All children are dead
+        unsafe { self.device.destroy_device(None) };
     }
 }
